42051e446d96b910c08ffded628f93d2f3925332
RuneWirtgen <runewirtgen@telenet.be>
2021-12-07 17:58:52 +0100

    Test

---
+++ guava-example/src/main/java/com/gusco/examples/guava_example/App.java
@@ -0,0 +1,14 @@
+package com.gusco.examples.guava_example;
+
+import com.google.common.collect.HashMultiset;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
